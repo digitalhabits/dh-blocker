e572507efe52263b709dcf239b3630900b59b1c1
ReDD Team <team@reddfocus.org>
2026-09-24 11:17:47 +0100

    Add Google's licence notice to the CityHash port
    - cityhash.rs ports Google's MIT-licensed city.cc but only named the licence, which requires its copyright and permission notice to be included
    - Adds the notice as the file header; no code change

--- src-tauri/src/profile_scan/cityhash.rs
+++ src-tauri/src/profile_scan/cityhash.rs
@@ -1,5 +1,25 @@
 // CityHash64 v1.0, which Firefox uses to name the `[Install<hash>]` sections
-// of profiles.ini. A port of Google's MIT-licensed city.cc.
+// of profiles.ini. A port of Google's city.cc, under its licence:
+//
+// Copyright (c) 2011 Google, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 const K0: u64 = 0xc3a5c85c97cb3127;
 const K1: u64 = 0xb492b66fbe98f273;
